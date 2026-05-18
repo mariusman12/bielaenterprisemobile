0a853c80872632285af15d2c5dd72cf4186e50e7
Man Marius <marius.man@titanlabs.tech>
2026-05-18 14:17:25 +0300

    fix zustand bug

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      function stripImportMetaEnv({ types: t }) {
+        return {
+          name: 'strip-import-meta-env',
+          visitor: {
+            MemberExpression(path) {
+              const { object, property } = path.node;
+              if (
+                t.isMetaProperty(object) &&
+                object.meta.name === 'import' &&
+                object.property.name === 'meta' &&
+                t.isIdentifier(property, { name: 'env' })
+              ) {
+                path.replaceWith(t.objectExpression([]));
+              }
+            },
+          },
+        };
+      },
+    ],
   };
 };
