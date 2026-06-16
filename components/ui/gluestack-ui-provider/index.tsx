a08ac429a124d53740a258e17293ff5d8ed48521
Man Marius <marius.man@titanlabs.tech>
2026-06-16 17:50:53 +0300

    fix darkmode 2

--- components/ui/gluestack-ui-provider/index.tsx
+++ components/ui/gluestack-ui-provider/index.tsx
@@ -1,26 +1,18 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { config } from './config';
 import { View, ViewProps } from 'react-native';
 import { OverlayProvider } from '@gluestack-ui/core/overlay/creator';
 import { ToastProvider } from '@gluestack-ui/core/toast/creator';
-import { useColorScheme } from 'nativewind';
 
 export type ModeType = 'light' | 'dark' | 'system';
 
 export function GluestackUIProvider({
-  mode = 'light',
   ...props
 }: {
   mode?: ModeType;
   children?: React.ReactNode;
   style?: ViewProps['style'];
 }) {
-  const { setColorScheme } = useColorScheme();
-
-  useEffect(() => {
-    setColorScheme('light');
-  }, []);
-
   return (
     <View
       style={[
